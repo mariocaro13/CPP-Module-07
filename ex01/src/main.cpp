ba8e6af7b543e807629f7574a991d2f1ff40df45
mariocaro13 <mariocaro2002@gmail.com>
2026-04-09 13:52:17 +0200

    Refactor print and increment functions into func namespace; update main logic to use new function signatures

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcaro-ro <mcaro-ro@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 12:28:17 by mcaro-ro          #+#    #+#             */
-/*   Updated: 2026/04/09 13:42:04 by mcaro-ro         ###   ########.fr       */
+/*   Updated: 2026/04/09 13:51:16 by mcaro-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 int main()
 {
 	int arr[] = {0, 1, 2, 3, 4, INT_MAX};
-	testNumericArray(arr, 10);
+	test::NumericArray(arr, 10);
 
 	unsigned int unsignedArr[] = {0, 1, 2, 3, 4, UINT_MAX};
-	testNumericArray(unsignedArr, 6);
+	test::NumericArray(unsignedArr, 6);
 
 	std::string strArr[] = {"Hello", "World", "!"};
-	::iter(strArr, 3, ::print<std::string>);
+	::iter(strArr, 3, func::print<std::string>);
 	std::cout << std::endl << std::endl;
 
 	const int constArr[] = {10, 20, 30, 40, 50};
-	::iter(constArr, 5, ::printConst<int>);
+	::iter(constArr, 5, func::printConst<int>);
 	std::cout << std::endl;
 
 	return (0);
